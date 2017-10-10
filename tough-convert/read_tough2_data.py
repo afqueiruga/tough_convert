1dbd27fcd48de351100c0740e486a88176b82616
Alejandro Francisco Queiruga <afq@berkeley.edu>
2017-10-10 14:55:38 -0700

    lda is different in tough2 than I tought. Does it vary???

--- tough-convert/read_tough2_data.py
+++ tough-convert/read_tough2_data.py
@@ -36,10 +36,13 @@ def load_tough2_output(fname, Nelem, Nconn, nameorder=None):
             # Split the pesky data
             #sp = re.sub(r"([^Ee])([-+])",r"\1 \2", l[6:]).split()[1:]
             sp = range(len(keys))
+            lda = 13
+            off = 12
             for j in range(len(keys)):
                 try:
-                    sp[j] = float(l[ (12+j*12) : (24+j*12) ])
+                    sp[j] = float(l[ (off+j*lda) : (off+(j+1)*lda) ])
                 except ValueError:
+                    print "Value error:", l[ (off+j*lda) : (off+(j+1)*lda) ]
                     sp[j] = 0.0
             # Save data
             if firsttime:
