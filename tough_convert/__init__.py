5b543367f2d23b3fa32ffce32c77d0d0c79f2d84
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-01-11 17:48:59 -0800

    Loading the library in the init

--- tough_convert/__init__.py
+++ tough_convert/__init__.py
@@ -1,4 +1,11 @@
 """
-Tough-convert
+Tough_convert
 """
 
+from meshmakerinputs import *
+from vtk_writer import *
+from silo_writer import *
+from gmsh_writer import *
+from flac3d_writer import *
+from read_tough_data import *
+from read_tough2_data import *
